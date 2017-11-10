f8dbd2353779976a5c793e8bb5b13ff035b9fccf
Arkadiusz Sojda <obs3rver@gmail.com>
2017-11-10 19:12:08 +0100

    Initial commit

---
+++ src/main/java/pl/artcoder/playground/videostore/VideostoreApplication.java
@@ -0,0 +1,12 @@
+package pl.artcoder.playground.videostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideostoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideostoreApplication.class, args);
+	}
+}
